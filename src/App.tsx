0c400d0770b186638c6ce26c69cff798c8c4dc67
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-12-08 16:54:35 -0300

    fix: changes App

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,34 @@ import { PlusCircle } from "phosphor-react";
 
 import styles from "./App.module.css";
 import "./global.css";
+import { TaskBox } from "./components/Header/TaskBox/TaskBox";
 
 export function App() {
   return (
-    <div>
+    <div className={styles.teste}>
       <Header />
 
-      <div className={styles.textareaAndButton}>
-        <textarea
-          className={styles.textarea}
-          placeholder="Adicione uma nova tarefa"
-        ></textarea>
-
-        <button className={styles.buttonCreate}>
-          <span className={styles.teste}>Criar</span>
-          <PlusCircle size={16} weight="bold" />
-        </button>
-      </div>
-
       <div>
-        <span>Tarefas criadas 0</span>
-        <span>Concluídas 0</span>
-      </div>
+        <div className={styles.textareaAndButton}>
+          <textarea
+            className={styles.textarea}
+            placeholder="Adicione uma nova tarefa"
+          ></textarea>
 
-      <div>
-        
+          <button className={styles.buttonCreate}>
+            <span>Criar</span>
+            <PlusCircle size={16} weight="bold" />
+          </button>
+        </div>
+
+        <div className={styles.informationsTask}>
+          <span className={styles.tasksCreated}>Tarefas criadas <span className={styles.numberTasksCreated}>0</span></span>
+          <span className={styles.completedTasks}>Concluídas <span className={styles.numberCompletedTasks}>0</span></span>
+        </div>
+
+        <div>
+          <TaskBox />
+        </div>
       </div>
     </div>
   );
