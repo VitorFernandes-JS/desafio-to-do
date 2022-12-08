e9b142b40ef7c9787e6548134f5cd78f7467ed27
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-12-08 12:18:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import styles from "./App.module.css";
+import "./App.module.css";
+
+export function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
