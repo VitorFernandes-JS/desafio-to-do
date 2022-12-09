7a76d62008e651af1ad57d8962bc60226f735b9e
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-12-09 11:17:48 -0300

    fix: change color body

--- src/components/Header/TaskBox/TaskBox.tsx
+++ src/components/Header/TaskBox/TaskBox.tsx
@@ -1,12 +1,14 @@
-import styles from './TaskBox.module.css';
-import { Trash } from 'phosphor-react';
+import styles from "./TaskBox.module.css";
+import { Trash } from "phosphor-react";
 
 export function TaskBox() {
-    return(
-        <div className={styles.container}>
-            <input type="checkbox" />
-            <h1>Arrumar o quarto</h1>
-            <Trash />
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <input type="checkbox" />
+      <h1>Arrumar o quarto</h1>
+      <button className={styles.buttonDeleteTask} title="Deletar task">
+        <Trash />
+      </button>
+    </div>
+  );
 }
